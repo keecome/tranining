c01a2b64ef1ef7685b15d372ca894653002bf27b
keecome <7017511@qq.com>
2018-12-07 09:32:14 +0000

    新建了培训模块

---
+++ ot_traning/__manifest__.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+{
+	'name':'PS Cloud培训练习模块',
+	'version':'12.0.1.0',
+	'summary':'广州救火 培训练习模块',
+	'author':"广州救火",
+	'website':'www.mypscloud.com',
+	'category':'Tools',
+	'depends':['base'],
+	'data':[
+        'views/res_partner_views.xml',
+        'views/training_lesson_views.xml',
+        'views/training_subject_views.xml',
+        'views/training_views.xml',
+	  ],
+	'demo':
+	  ],
+	'qweb':[],
+	'js':[],
+	'css':[],
+	'auto_install':False,
+	'application':True,
+}
